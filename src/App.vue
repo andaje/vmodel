a14a578971cec9884c375926f199021c4f323d15
Tom <syntraprogrammeurs@gmail.com>
2019-01-24 11:07:13 +0100

    vmodel Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,123 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div class="container">
+          <div class="row">
+              <div class="col">
+                  <div class="card">
+                      <h4 class="card-header" :class="cardClasses">
+                      Class Binding
+                        </h4>
+                      <div class="card-body">
+                          <div class="form-group">
+                              <select class="form-control" v-model="card.selectedStyling">
+                                  <option v-for="style in card.contextualStyles" :value="style">
+                                      {{ style.type | ucfirst }}
+                                  </option>
+                              </select>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col">
+                  <div class="card">
+                      <h4 class="card-header">
+                          Binding Style
+                      </h4>
+                      <div class="card-body">
+                          <div class="form-group">
+                              <div class="input-group">
+                                  <input class="form-control" v-model="progress.width"
+                                         placeholder="breedte progressbar in pct" type="text">
+                                  <span class="input-group-addon">%</span>
+                              </div>
+                          </div>
+                          <div class="form-group">
+                              <input class="form-control" v-model="progress.backgroundcolor" type="text">
+                          </div>
+                      </div>
+                      <div class="card-footer">
+                          <div class="progress">
+                              <div class="progress-bar" :style="progressStyling">
+                              
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
+    /* applicatie naam */
   name: 'app',
-  components: {
-    HelloWorld
-  }
+    /* view model die door App.vue wordt gebruikt*/
+    /* @returns {object} van het view-model retourneren*/
+  data(){
+      return{
+          /**card**/
+          card:{
+              selectedStyling:{
+                  type:'light',
+                  hasWhiteText:false
+              },
+              contextualStyles:[
+                  { type: 'primary', hasWhiteText: true,},
+                  { type: 'secondary', hasWhiteText: true,},
+                  { type: 'success', hasWhiteText: true,},
+                  { type: 'danger', hasWhiteText: true,},
+                  { type: 'warning', hasWhiteText: true,},
+                  { type: 'info', hasWhiteText: true,},
+                  { type: 'light', hasWhiteText: true,},
+                  { type: 'dark', hasWhiteText: true,},
+              ]
+          },
+          
+          /** progress bar*/
+          progress:{
+              width:50,
+              backgroundcolor: '#0af'
+          }
+      }
+  },
+    computed:{
+      /* hier gaan we de classes coderen voor het wijzigen van kleur in de card-header*/
+        cardClasses(){
+            const classes = [];
+            
+            if(!this.card.selectedStyling){
+                return classes;
+            }
+            classes.push(`bg-${this.card.selectedStyling.type}`);
+            
+            if(this.card.selectedStyling.hasWhiteText){
+                classes.push('text-white');
+            }
+            return classes;
+            
+        },
+        progressStyling(){
+            return{
+                width: `${this.progress.width}%`,
+                backgroundColor: `#${this.progress.backgroundcolor}`
+            }
+        }
+    },
+    
+    filters:{
+      /*eerste letter in hoofdletters*/
+        ucfirst(value){
+            if(!value){
+                return '';
+            }
+            return value.charAt(0).toUpperCase() + value.slice(1);
+        }
+    }
+ 
 }
 </script>
 
